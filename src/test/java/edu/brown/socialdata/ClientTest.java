e7ad1dd9ee36bd1061c9a3256bdff21b618030c0
tylerfox <tyler_fox@brown.edu>
2015-03-14 15:01:49 -0400

    Added testing packages for networking and social|

---
+++ src/test/java/edu/brown/socialdata/ClientTest.java
@@ -0,0 +1,34 @@
+package edu.brown.socialdata;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ClientTest {
+
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+    // (Optional) Code to run before any tests begin goes here.
+  }
+
+  @AfterClass
+  public static void tearDownClass() throws Exception {
+    // (Optional) Code to run after all tests finish goes here.
+  }
+
+  @Before
+  public void setUp() {
+    // (Optional) Code to run before each test case goes here.
+  }
+
+  @After
+  public void tearDown() {
+    // (Optional) Code to run after each test case goes here.
+  }
+
+  @Test
+  public void test() {
+  }
+
+}
