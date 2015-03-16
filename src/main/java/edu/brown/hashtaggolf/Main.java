896bb839d1b15cdcff93f167cb572cdc6f6c998f
Beverly <beverly_tai@brown.edu>
2015-03-15 23:46:40 -0400

    Updated Game UML

--- src/main/java/edu/brown/hashtaggolf/Main.java
+++ src/main/java/edu/brown/hashtaggolf/Main.java
@@ -1,30 +1,12 @@
 package edu.brown.hashtaggolf;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Map;
-
-import spark.ExceptionHandler;
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.Spark;
-import spark.TemplateViewRoute;
-import spark.template.freemarker.FreeMarkerEngine;
-
-import com.google.common.collect.ImmutableMap;
-
 import edu.brown.socialdata.TwitterQuery;
-import freemarker.template.Configuration;
+
 
 /**
  * The main stars class that is used to invoke the program.
  */
 public class Main {
-
-  private static final int ERROR_STATUS = 500;
 
   /**
    * standard main method.
@@ -46,65 +28,4 @@ public class Main {
     tq.getCount("#DaylightSavingTime", 300);
   }
 
-  /**
-   * Creates spark engine.
-   *
-   * @return the freemarkerengine
-   */
-  private static FreeMarkerEngine createEngine() {
-    Configuration config = new Configuration();
-    File templates = new File("src/main/resources/spark/template/freemarker");
-    try {
-      config.setDirectoryForTemplateLoading(templates);
-    } catch (IOException ioe) {
-      System.out.printf("ERROR: Unable use %s for template loading.%n",
-          templates);
-      System.exit(1);
-    }
-    return new FreeMarkerEngine(config);
-  }
-
-  /**
-   * Runs spark server.
-   */
-  private void runSparkServer() {
-    Spark.externalStaticFileLocation("src/main/resources/static");
-    Spark.exception(Exception.class, new ExceptionPrinter());
-
-    FreeMarkerEngine freeMarker = createEngine();
-
-    // Setup Spark Routes
-    Spark.get("/", new FrontHandler(), freeMarker);
-  }
-
-  /**
-   * Handler for the 'home' page /stars.
-   *
-   */
-  private static class FrontHandler implements TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = new ImmutableMap.Builder<String, Object>()
-          .put("title", "#Golf").build();
-      return new ModelAndView(variables, "main.ftl");
-    }
-  }
-
-  /**
-   * Prints exceptions to the gui.
-   *
-   */
-  private static class ExceptionPrinter implements ExceptionHandler {
-    @Override
-    public void handle(Exception e, Request req, Response res) {
-      res.status(ERROR_STATUS);
-      StringWriter stacktrace = new StringWriter();
-      try (PrintWriter pw = new PrintWriter(stacktrace)) {
-        pw.println("<pre>");
-        e.printStackTrace(pw);
-        pw.println("</pre>");
-      }
-      res.body(stacktrace.toString());
-    }
-  }
 }
--- src/main/java/edu/brown/hashtaggolf/State.java
+++
@@ -1,9 +0,0 @@
-package edu.brown.hashtaggolf;
-
-public class State {
-
-  /**
-   * Getters and setters for info about game state like position of ball, HUD
-   * info etc.
-   */
-}
